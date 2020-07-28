c299525913f958ac851f6cecc7539e199d460c9e
wushengran <shengran.wu@163.com>
2020-07-28 12:16:07 +0800

    Day 8 am: set & map & tuple

--- src/main/scala/com/atguigu/chapter7/Test03_MulArray.scala
+++ src/main/scala/com/atguigu/chapter7/Test03_MulArray.scala
@@ -32,6 +32,24 @@ object Test03_MulArray {
     arr.foreach( x => x.foreach(println) )
     arr.foreach( _.foreach(println) )
 
+    println("=====================================")
+
     // 作业：创建一个五维数组 2*3*4*5*6，并遍历
+
+    val fivedimArr: Array[Array[Array[Array[Array[Int]]]]] = Array.ofDim[Int](2,3,4,5,6)
+    for{i <- fivedimArr.indices
+         j <- fivedimArr(i).indices
+         k <- fivedimArr(i)(j).indices
+         m <- fivedimArr(i)(j)(k).indices
+         n <- fivedimArr(i)(j)(k)(m).indices
+    }{
+      fivedimArr(i)(j)(k)(m)(n) = i * j * k * m * n
+      //      println(fivedimArr(i)(j)(k)(m)(n))
+    }
+
+    println("======================")
+
+    fivedimArr.foreach( _.foreach( _.foreach( _.foreach( _.foreach(println) ) ) ) )
+
   }
 }
