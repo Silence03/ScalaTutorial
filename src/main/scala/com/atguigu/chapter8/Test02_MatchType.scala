afedb4e9ba1ebfbf72b404807bc6a8a0f03524a5
wushengran <shengran.wu@163.com>
2020-07-29 17:09:28 +0800

    Day 8 pm: pattern match & others

--- src/main/scala/com/atguigu/chapter8/Test02_MatchType.scala
+++ src/main/scala/com/atguigu/chapter8/Test02_MatchType.scala
@@ -22,6 +22,7 @@ object Test02_MatchType {
       case '+' => "Char +"
       case _ =>
     }
+
     println(describeConst(true))
     println(describeConst(5))
     println(describeConst("hello"))
@@ -39,17 +40,18 @@ object Test02_MatchType {
       // 默认情况：一般是用_，也可以用一个变量（没有类型）来接收当前的值
       case abc => "something else " + abc
     }
+
     println(describeType(23))
     println(describeType("hello"))
-    println(describeType(List("a","b","c")))
-    println(describeType(List(1,2,3,4)))    // 正常匹配List，泛型擦除
-    println(describeType(Array("a","b","c")))
-    println(describeType(Array(1,2,3,4)))    // Array[Int] --> int[]
+    println(describeType(List("a", "b", "c")))
+    println(describeType(List(1, 2, 3, 4))) // 正常匹配List，泛型擦除
+    println(describeType(Array("a", "b", "c")))
+    println(describeType(Array(1, 2, 3, 4))) // Array[Int] --> int[]
 
     println("================================")
 
     // 3. 匹配数组
-    for( arr <- List(
+    for (arr <- List(
       Array(0),
       Array(0, 1),
       Array(1, 0),
@@ -57,14 +59,59 @@ object Test02_MatchType {
       Array(1, 1, 1),
       Array(2, 15, 37),
       Array("hello", 56, 7.9)
-    ) ){
+    )) {
       val res = arr match {
         case Array(0) => "0"
-        case Array(0, _) => "以0开头的二元组"
-        case Array(0, _*) => "以0开头的元组"
-        case Array(_, 1, _*) => "第二个元素为1的元组"
+        case Array(0, _) => "以0开头的二元数组"
+        case Array(0, _*) => "以0开头的数组"
+        case Array(_, 1, _*) => "第二个元素为1的数组"
         case Array(x, y) => x + ", " + y
         case _ => "其它情况"
+      }
+      println(res)
+    }
+
+    println("=============================")
+    // 4. 匹配列表
+    // 方式1：直接定义List的形式
+    for (list <- List(
+      List(0),
+      List(25),
+      List(0, 1),
+      List(1, 0),
+      List(0, 1, 0),
+      List(1, 1, 1),
+      List(2, 15, 37),
+      List("hello", 56, 7.9))) {
+      val res = list match {
+        case List(0) => "0"
+        case List(a, b) => a + ", " + b
+        case List(0, _*) => "0 ..."
+        case List(a) => a
+        case _ => "something else"
+      }
+      println(res)
+    }
+
+    println("============================")
+
+    // 方式2：用双冒号::构建list形式
+    val list = List(2, 1)
+    list match {
+      case first :: 1 :: rest => println("第二个元素是1")
+      case first :: second :: rest => println("至少有两个元素： " + first + second + rest)
+      case _ => println("其它情况")
+    }
+
+    // 5. 匹配元组
+    for( tuple <- List(
+      (0,1), (0,0), (1,0), (1,1), (1,0,2)
+    ) ){
+      val res = tuple match {
+        case (0, _) => "0 .."   // 第一个元素是0的二元组
+        case (y, 0) => y + ", 0"    // 第二元素是0的对偶元组
+        case (a, b) => a + " " + b    // 普通二元组
+        case _ => "something else"    // 默认
       }
       println(res)
     }
